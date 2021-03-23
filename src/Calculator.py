a289e0e5dc583d1b081face6916d17e2c559ff46
mrr28 <mrr28@njit.edu>
2021-03-22 23:19:24 -0400

    Feature: add in tests for multiply and divide

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,16 +4,30 @@ def addition(a, b):
 def subtraction(a, b):
     return a - b
 
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a // b
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
 
-    def  add(self, a, b):
+    def add(self, a, b):
         self.result = a + b
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
+        self.result = a - b
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = a * b
+        return self.result
+
+    def divide(self, a, b):
+        self.result = a // b
         return self.result
